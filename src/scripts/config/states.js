f0d876ce9d90e79bb5a7dcfb657e4b8970c09f2f
Federico Scarpa <fedescarpa@gmail.com>
2016-10-31 04:37:36 -0300

    added basic navbar

--- src/scripts/config/states.js
+++ src/scripts/config/states.js
@@ -8,9 +8,12 @@ angular
         views: {
           '@': {
             templateUrl: 'views/layout.html',
+          },
+          'navbar@editor': {
+            templateUrl: 'views/navbar/navbar.html',
           }
         }
-      })
+      });
 
     $urlRouterProvider.otherwise(($injector) => {
       $injector.get('$state').go('editor', {}, { reload: true, location: 'replace' });
