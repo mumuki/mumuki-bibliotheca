6060cbc8d2f5b8785c455d0f1f357cfee964f030
Federico Scarpa <fedescarpa@gmail.com>
2016-10-30 23:54:51 -0300

    added images task

--- tasks/dev.js
+++ tasks/dev.js
@@ -16,13 +16,14 @@ module.exports = (done) => {
 
 
 gulp.task('dev:build', (done) => {
-  runs('dev:clean', 'dev:scripts', 'dev:styles', 'dev:views', 'dev:fonts', done);
+  runs('dev:clean', 'dev:scripts', 'dev:styles', 'dev:views', 'dev:fonts', 'dev:images', done);
 });
 
 gulp.task('dev:watch', () => {
   gulp.watch(`${srcFolder}/index.jade`, ['dev:views:index']);
   gulp.watch(`${srcFolder}/views/**/*`, ['dev:views:jade']);
   gulp.watch(`${srcFolder}/fonts/**/*`, ['dev:fonts']);
+  gulp.watch(`${srcFolder}/images/**/*`, ['dev:images']);
   gulp.watch(`${srcFolder}/styles/**/*`, ['dev:styles']);
   gulp.watch(`${srcFolder}/scripts/**/*`, ['dev:scripts']);
   gulp.watch(`${srcFolder}/bower_components/**/*`, ['dev:views:index']);
@@ -66,6 +67,11 @@ gulp.task('dev:views', (done) => {
 gulp.task('dev:fonts', function () {
   return gulp.src(`${srcFolder}/fonts/**/*`)
     .pipe(gulp.dest(`${outFolder}/fonts`));
+});
+
+gulp.task('dev:images', function () {
+  return gulp.src(`${srcFolder}/images/**/*`)
+    .pipe(gulp.dest(`${outFolder}/images`));
 });
 
 gulp.task('dev:views:index', () => {
