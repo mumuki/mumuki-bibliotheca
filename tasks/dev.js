6c376278abf00b653ab73260507e3b23ebc5f81f
Federico Scarpa <fedescarpa@gmail.com>
2016-10-30 23:31:21 -0300

    added mumuki-fonts

--- tasks/dev.js
+++ tasks/dev.js
@@ -16,12 +16,13 @@ module.exports = (done) => {
 
 
 gulp.task('dev:build', (done) => {
-  runs('dev:clean', 'dev:scripts', 'dev:styles', 'dev:views', done);
+  runs('dev:clean', 'dev:scripts', 'dev:styles', 'dev:views', 'dev:fonts', done);
 });
 
 gulp.task('dev:watch', () => {
   gulp.watch(`${srcFolder}/index.jade`, ['dev:views:index']);
   gulp.watch(`${srcFolder}/views/**/*`, ['dev:views:jade']);
+  gulp.watch(`${srcFolder}/fonts/**/*`, ['dev:fonts']);
   gulp.watch(`${srcFolder}/styles/**/*`, ['dev:styles']);
   gulp.watch(`${srcFolder}/scripts/**/*`, ['dev:scripts']);
   gulp.watch(`${srcFolder}/bower_components/**/*`, ['dev:views:index']);
@@ -60,6 +61,11 @@ gulp.task('dev:styles', function () {
 
 gulp.task('dev:views', (done) => {
   runs(['dev:views:index'], done);
+});
+
+gulp.task('dev:fonts', function () {
+  return gulp.src(`${srcFolder}/fonts/**/*`)
+    .pipe(gulp.dest(`${outFolder}/fonts`));
 });
 
 gulp.task('dev:views:index', () => {
