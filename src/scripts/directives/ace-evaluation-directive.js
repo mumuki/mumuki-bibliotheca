b55d34cce4c79cef3673e1d93a22d6da884f83b0
Federico Scarpa <fedescarpa@gmail.com>
2016-11-24 15:17:07 -0300

    refactor evaluations tabs directive

--- src/scripts/directives/ace-evaluation-directive.js
+++ src/scripts/directives/ace-evaluation-directive.js
@@ -11,18 +11,42 @@ angular
       },
       controller: ($scope) => {
 
+        let _selectedTab;
+
         $scope.tabs = [
-          { name: 'test', selected: true },
-          { name: 'expectations', selected: false },
-          { name: 'extra', selected: false },
-          { name: 'default_code', selected: false },
-          { name: 'solution', selected: false },
+          {
+            name: 'test',
+            templateUrl: 'views/directives/evaluation/test.html',
+          },
+          {
+            name: 'expectations',
+            templateUrl: 'views/directives/evaluation/expectations.html',
+          },
+          {
+            name: 'extra',
+            templateUrl: 'views/directives/evaluation/extra.html',
+          },
+          {
+            name: 'default_code',
+            templateUrl: 'views/directives/evaluation/default_code.html',
+          },
+          {
+            name: 'solution',
+            templateUrl: 'views/directives/evaluation/solution.html',
+          },
         ];
 
         $scope.selectTab = (tab) => {
           $scope.tabs.forEach((t) => t.selected = false);
           tab.selected = true;
+          _selectedTab = tab;
         }
+
+        $scope.isSelected = (name) => {
+          return { "active": _selectedTab.name === name };
+        }
+
+        $scope.selectTab($scope.tabs[0]);
 
       }
 
