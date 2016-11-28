99c3dfd3035d228048f4a7028704f8619e10e2ce
Federico Scarpa <fedescarpa@gmail.com>
2016-11-28 18:53:46 -0300

    added language model tests

---
+++ specs/models/language-model.spec.js
@@ -0,0 +1,19 @@
+
+editorTest('Language Model', () => {
+
+  let Language;
+
+  beforeSpec((_Language_) => Language = _Language_);
+
+  context('#icon', () => {
+    spec(() => Language.from({ name: 'foo' }).icon().should.be.eql('da-foo'));
+  });
+
+  context('#sortBy', () => {
+    spec(() => Language.sortBy().should.be.an.Array);
+    spec(() => Language.sortBy().length.should.be.eql(1));
+    spec(() => Language.sortBy()[0].should.be.eql('name'));
+  });
+
+});
+
