3732f1473a8d0982afab0c3d00250a2d133e0800
Federico Scarpa <fedescarpa@gmail.com>
2016-12-12 12:17:57 -0300

    added production config

---
+++ config/production.js
@@ -0,0 +1,15 @@
+
+angular
+  .module('editor')
+  .constant('CONFIG', {
+
+    bibliotheca: {
+      host: 'http://bibliotheca.mumuki.io'
+    }
+
+    auth: {
+      domain: 'mumuki.auth0.com',
+      clientID: '08Bvp9bczfIb0Ps6yZ52Pl0H7CzukglU'
+    },
+
+  });
