e33ec51b5566b8429e13ee3af3245e553a728992
Federico Scarpa <fedescarpa@gmail.com>
2016-12-12 13:15:39 -0300

    added production gulp tasks

--- config/production.js
+++ config/production.js
@@ -1,15 +1,14 @@
-
 angular
   .module('editor')
   .constant('CONFIG', {
 
     bibliotheca: {
       host: 'http://bibliotheca.mumuki.io'
-    }
+    },
 
     auth: {
       domain: 'mumuki.auth0.com',
       clientID: '08Bvp9bczfIb0Ps6yZ52Pl0H7CzukglU'
-    },
+    }
 
   });
