4551cb26e2063465287862fd84acf0ad732dba6f
Agustin Pina <aguspina87@gmail.com>
2017-02-13 16:37:53 -0300

    Fix test config

--- config/test.js
+++ config/test.js
@@ -5,4 +5,10 @@ angular
     bibliotheca: {
       url: 'bibliotheca-api.localmumuki.io:9292'
     },
+
+    cookie: {
+      domain: '.localmumuki.io',
+      session: '_mumuki_bibliotheca_session',
+      regular: 'mucookie'
+    }
   });
