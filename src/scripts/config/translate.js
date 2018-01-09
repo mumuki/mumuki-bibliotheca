858f125e58be67b4ecdae8e7918d06207c248ec1
Julian Berbel Alt <julianberbel@hotmail.com>
2018-01-09 12:44:27 -0300

    Add portuguese to locales list

--- src/scripts/config/translate.js
+++ src/scripts/config/translate.js
@@ -3,11 +3,13 @@ angular
   .config(function ($injector, $translateProvider) {
     const LANG_ES = $injector.get('LANG_ES');
     const LANG_EN = $injector.get('LANG_EN');
+    const LANG_PT = $injector.get('LANG_PT');
 
     const supportedLocales = ['es', 'en'];
 
     $translateProvider.translations('es', LANG_ES);
     $translateProvider.translations('en', LANG_EN);
+    $translateProvider.translations('pt', LANG_PT);
 
     $translateProvider.determinePreferredLanguage(() => {
       const locale = $translateProvider.resolveClientLocale().split('_')[0];
