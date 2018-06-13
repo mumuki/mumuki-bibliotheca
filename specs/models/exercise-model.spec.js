93ac5172ea39dd9475da8008745d33cbbd639b6d
Federico Scarpa <fedescarpa@gmail.com>
2018-06-13 18:20:46 -0300

    fixing test

--- specs/models/exercise-model.spec.js
+++ specs/models/exercise-model.spec.js
@@ -164,7 +164,8 @@ editorTest('Exercise Model', () => {
     spec(() => exercise.should.have.property('editor').eql('code'));
     spec(() => exercise.should.have.property('layout').eql('input_right'));
     spec(() => exercise.should.have.property('test').eql(''));
-    spec(() => _.keys(exercise).length.should.be.eql(5));
+    spec(() => exercise.should.have.property('assistance_rules'));
+    spec(() => _.keys(exercise).length.should.be.eql(6));
   });
 
 });
