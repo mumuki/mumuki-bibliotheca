455d66cd406301ab05e8b71730ef18ca8b01dd54
Rodrigo Alfonso <rodri042@gmail.com>
2018-06-15 02:47:18 -0300

    Auto-opening foldables with content

---
+++ specs/services/foldable-service.spec.js
@@ -0,0 +1,26 @@
+
+editorTest('Foldable Service', (mocks) => {
+
+  var Foldable, $scope;
+
+  beforeSpec((_Foldable_, _$rootScope_) => {
+    Foldable = _Foldable_;
+    $scope = _$rootScope_;
+  });
+
+  const foldableWith = (content) => {
+    $scope.content = content;
+    Foldable.from($scope);
+    return $scope;
+  };
+
+  it('starts folded when the field is empty', () => {
+    foldableWith('').isMinimized().should.be.ok;
+  });
+
+  it('starts unfolded when the field has content', () => {
+    foldableWith('asd').isMinimized().should.not.be.ok;
+  });
+
+});
+
